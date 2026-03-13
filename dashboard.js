2921fcc42cc402a30054bccdeb4f7d50963347c4
mdswoponsheik <mdswoponshek1226@gmail.com>
2026-03-14 02:08:47 +0600

    modal and labels's array  complete

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,70 @@
  
- const conunt = document.getElementById("issues-container")
- const count =conunt.length 
- console.log(count);
+ const labelsElement = (arr) => {
+    const mapping = arr.map((lab) => `<button class="btn text-xl text-[#D97706] bg-[#FDE68A50] border-[#D9770670] rounded-4xl ">${lab}</button>`);
+    return mapping.join(" ");
+ };
  
- 
+ const remover = () => {
+    const removerBtn = document.querySelectorAll(".remove")
+    removerBtn.forEach((btn) => btn.classList.remove("active"));
+ }
+
+
+ const loadMolad = async (id) => {
+    const url = ` https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res =await fetch(url);
+    const detail = await res.json();
+    displayModal(detail.data);
+ }
+
+  displayModal = (word) =>{
+    console.log(word);
+    const boxModal =document.getElementById("detail-container");
+    boxModal.innerHTML =`
+         
+                <h2 class="text-3xl font-bold" >${word.title}</h2>
+                <div class="flex items-center space-x-8 text-xl">
+                    
+                    <button class="btn text-white bg-green-600 rounded-4xl">${word.status}</button>
+                    <li>opened</li>
+                    <li>21/2/200</li>
+                </div>
+                <div class="">
+                    <div class="flex gap-3 m-5">
+                    <div>
+                        ${labelsElement(word.labels)}
+                    </div>
+                    
+
+                </div>
+                <p class="text-2xl">${word.description}</p>
+                </div>
+                <div class="flex bg-gray-100 rounded-xl p-3">
+                    <div class="w-1/2">
+                        <h3 class="text-gray-300">assigenee:</h3>
+                        <h3 class="text-gray-400">${word.author}</h3>
+                    </div>
+                    <div class="w-1/2">
+                        <h3>priority:</h3>
+                        <button class="btn text-xl   text-white bg-red-500 rounded-4xl ">${word.priority}</button>
+                    </div>
+                 
+    `
+
+//     "title": "Add bulk operations support",
+// "description": "Allow users to perform bulk actions like delete, update status on multiple items at once.",
+// "status": "open",
+// "labels": [
+// "enhancement"
+// ],
+// "priority": "low",
+// "author": "bulk_barry",
+// "assignee": "",
+// "createdAt": "2024-02-02T10:00:00Z",
+// "updatedAt": "2024-02-02T10:00:00Z"
+
+    document.getElementById("my_modal_5").showModal();
+  }
  
  
  
@@ -13,7 +74,9 @@
             .then((res) => res.json())
             .then((json) => {
                 const clickbtn = document.getElementById("loadAll")
+                remover();
                 clickbtn.classList.add("active");
+                document.getElementById("box-counter").innerText = json.data.length;
                 displayAll(json.data)
             });
         };
@@ -38,17 +101,14 @@
                 </div>
                 <div class="mx-5">
                       <div class="">
-                    <h2 class="text-4xl font-bold my-auto mb-3"> ${all.title} </h2>
+                    <h2 onclick="loadMolad(${all.id})" class="text-4xl font-bold my-auto mb-3"> ${all.title} </h2>
                     <p> ${all.description} </p>
                 </div>
                 <div class="flex gap-3 m-5">
+                    
                     <div>
 
-                       <button class="btn text-xl   text-red-500 bg-[#FECACA50] border-red-400 rounded-4xl ">bug</button>
-                    </div>
-                    <div>
-
-                        <button class="btn text-xl text-[#D97706] bg-[#FDE68A50] border-[#D9770670] rounded-4xl ">helpdjhxzdfhjn wafdsf</button>
+                       ${labelsElement(all.labels)}
                     </div>
                     <!-- <div>
 
@@ -83,8 +143,10 @@
             .then((res) => res.json())
             .then((data) => {
                 const clickbtn = document.getElementById("loadOpen")
+                remover();
                 clickbtn.classList.add("active");
                 const openIssues = data.data.filter(all => all.status === "open");
+                document.getElementById("box-counter").innerText = openIssues.length;
                 displayOpen(openIssues);
             });
         };
@@ -105,17 +167,12 @@
                 </div>
                 <div class="mx-5">
                       <div class="">
-                    <h2 class="text-4xl font-bold my-auto mb-3"> ${open.title} </h2>
+                    <h2 onclick="loadMolad(${open.id})" class="text-4xl font-bold my-auto mb-3"> ${open.title} </h2>
                     <p> ${open.description} </p>
                 </div>
                 <div class="flex gap-3 m-5">
                     <div>
-
-                       <button class="btn text-xl   text-red-500 bg-[#FECACA50] border-red-400 rounded-4xl ">bug</button>
-                    </div>
-                    <div>
-
-                        <button class="btn text-xl text-[#D97706] bg-[#FDE68A50] border-[#D9770670] rounded-4xl ">helpdjhxzdfhjn wafdsf</button>
+                        ${labelsElement(open.labels)}
                     </div>
                     <!-- <div>
 
@@ -150,8 +207,10 @@
             .then((res) => res.json())
             .then((data) => {
                 const clickbtn = document.getElementById("loadClosed")
+                remover();
                 clickbtn.classList.add("active");
                 const openIssues = data.data.filter(all => all.status === "closed");
+                document.getElementById("box-counter").innerText = openIssues.length;
                 displayClosed(openIssues);
             });
         };
@@ -164,7 +223,7 @@
                 
                 const closedDiv =document.createElement("div")
                 closedDiv.innerHTML= `
-                <div class="rounded-2xl shadow-lx  p-3 h-100">
+                <div class="rounded-2xl shadow-xl  p-3 h-100">
                 <div class="flex justify-between m-5">
                     <img class="w-10" src="./assets/Open-Status.png" alt="">
                 <button class="btn  text-red-500 bg-[#FECACA50] rounded-3xl "> ${closed.priority} </button>
@@ -172,18 +231,13 @@
                 </div>
                 <div class="mx-5">
                       <div class="">
-                    <h2 class="text-4xl font-bold my-auto mb-3"> ${closed.title} </h2>
+                    <h2 onclick="loadMolad(${closed.id})" class="text-4xl font-bold my-auto mb-3"> ${closed.title} </h2>
                     <p> ${closed.description} </p>
                 </div>
                 <div class="flex gap-3 m-5">
-                    <div>
-
-                       <button class="btn text-xl   text-red-500 bg-[#FECACA50] border-red-400 rounded-4xl ">bug</button>
-                    </div>
-                    <div>
-
-                        <button class="btn text-xl text-[#D97706] bg-[#FDE68A50] border-[#D9770670] rounded-4xl ">helpdjhxzdfhjn wafdsf</button>
-                    </div>
+                    <div>     
+                        ${labelsElement(closed.labels)}              
+                     </div>
                     <!-- <div>
 
                         <button class="btn text-[#00A96E] bg-[#BBF7D050] rounded-4xl">help wafdsf</button>
